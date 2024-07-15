3cdcdd33c18ce8f664c447db2ff76339c44ac22b
Claude-Marcel L. Karlsson <whoismarcel@gmail.com>
2024-07-15 03:40:22 +0200

    yes

---
+++ src/index.ts
@@ -0,0 +1,13 @@
+import { ApolloServer } from "@apollo/server";
+import { startStandaloneServer } from "@apollo/server/standalone";
+import { typeDefs } from "./schema";
+
+const server = new ApolloServer({
+    typeDefs,
+});
+
+const { url } = await startStandaloneServer(server, {
+    listen: { port: 4000 },
+});
+
+console.log(`Server live at port`, 4000);
